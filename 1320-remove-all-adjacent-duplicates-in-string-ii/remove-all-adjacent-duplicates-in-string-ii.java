61a12fe10369b9caf444f9dca94f14fe405fc572
Shaurya Kundan <84137960+shauryaverma1005@users.noreply.github.com>
2026-07-08 17:48:44 +0530

    Time: 253 ms (5.04%) | Memory: 48.8 MB (10.85%) - LeetSync

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,37 @@
+class Pair{
+    char ch;
+    int freq;
+    Pair(char ch, int freq){
+        this.ch = ch;
+        this.freq = freq;
+    }
+}
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        int n = s.length();
+        Stack<Pair> stack = new Stack<>();
+        String res = "";
+        for(int i=0; i<n; i++){
+            char ch = s.charAt(i);
+
+            if(!stack.isEmpty() && stack.peek().ch == ch){
+                if(stack.peek().freq == k-1){
+                    stack.pop();
+                    continue;
+                }
+                stack.peek().freq++;
+            } else {
+                stack.push(new Pair(ch, 1));
+            }
+        }
+        while(!stack.isEmpty()){
+            Pair top = stack.pop();
+            int freq = top.freq;
+            while(freq!=0){
+                res = top.ch +res;
+                freq--;
+            }
+        }
+        return res;
+    }
+}
