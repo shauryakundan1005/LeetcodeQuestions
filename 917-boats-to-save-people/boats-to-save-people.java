e91ddbd6021d4671589dcc51955257bfadef7456
Shaurya Kundan <84137960+shauryaverma1005@users.noreply.github.com>
2026-07-16 15:00:17 +0530

    Time: 19 ms (98.43%) | Memory: 56.6 MB (37.93%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -4,7 +4,7 @@ class Solution {
         int n = people.length;
         int i = 0, j = n-1;
         int count = 0;
-        while(i<n && i<=j){
+        while(i<=j){
             if((people[i] + people[j])<= limit){
                 count++;
                 i++;
