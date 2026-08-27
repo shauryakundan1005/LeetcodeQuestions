244338b065d2d8be0c76d57ee3777181c2a04d4c
Shaurya Kundan <84137960+shauryakundan1005@users.noreply.github.com>
2026-08-27 12:08:22 +0530

    Time: 21 ms (7.36%) | Memory: 44.3 MB (74.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,18 +4,16 @@ class Solution {
         int m = t.length();
         if(n!=m){ return false;}
         HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> map1 = new HashMap<>();
         for(int i=0; i<n;i++){
             char ch = s.charAt(i);
             char ch2 = t.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0)+1);
-            map1.put(ch2, map1.getOrDefault(ch2, 0)+1);
+            map.put(ch2, map.getOrDefault(ch2, 0)-1);
         }
-        for(int i = 0; i<n; i++){
-            char ch = s.charAt(i);
-            if (!map.get(s.charAt(i)).equals(map1.get(s.charAt(i)))) {
-    return false;
-}
+        for(int value: map.values()){
+            if(value != 0){
+                return false;
+            }
         }
         return true;
     }
